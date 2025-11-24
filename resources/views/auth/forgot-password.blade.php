a54f949380bb247bbfbe9bfdb4287643b785a078
Dhiauddin <dhiauddin@gmail.com>
2025-11-24 18:38:09 +0800

    --

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -2,22 +2,29 @@
 
 @section('content')
 <div class="col-sm-12 d-flex flex-column align-items-center justify-content-center">
-	<h3>Forgot your password? No problem. Just let us know your username and we will email you a password reset link that will allow you to choose a new one.</h3>
 	<form method="POST" action="{{ route('password.email') }}" id="form" class="needs-validation">
 		@csrf
 
-		<div class="form-group row m-2 @error('username') has-error @enderror">
-			<label for="username" class="col-sm-4 col-form-label col-form-label-sm">Username : </label>
-			<div class="col-sm-8">
-				<input type="text" name="username" id="username" value="{{ old('username') }}" class="form-control form-control-sm @error('username') is-invalid @enderror" placeholder="Username">
-				@error('username') <div class="invalid-feedback fw-lighter">{{ $message }}</div> @enderror
+		<div class="card">
+			<div class="card-header">
+				<h3>Forgot your password? No problem. Just let us know your username and we will email you a password reset link that will allow you to choose a new one.</h3>
 			</div>
-		</div>
-
-		<div class="m-2">
-			<button type="submit" class="btn btn-sm btn-primary m-3">
-				{{ __('Password Reset Link') }}
-			</button>
+			<div class="card-body">
+				<div class="form-group row m-2 @error('username') has-error @enderror">
+					<label for="username" class="col-sm-4 col-form-label col-form-label-sm">Username : </label>
+					<div class="col-sm-8 my-auto">
+						<input type="text" name="username" id="username" value="{{ old('username') }}" class="form-control form-control-sm @error('username') is-invalid @enderror" placeholder="Username">
+						@error('username') <div class="invalid-feedback fw-lighter">{{ $message }}</div> @enderror
+					</div>
+				</div>
+			</div>
+			<div class="card-footer text-end">
+				<div class="m-0">
+					<button type="submit" class="btn btn-sm btn-primary m-3">
+						{{ __('Password Reset Link') }}
+					</button>
+				</div>
+			</div>
 		</div>
 	</form>
 </div>
