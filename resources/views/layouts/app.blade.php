04ad9231d31cb27f03a621f58047af80cf837934
Dhiauddin <dhiauddin@gmail.com>
2026-02-19 17:39:36 +0800

    -- update app.blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@ $currentYear = Carbon::now()->year;
 	@vite(['resources/scss/app.scss', 'resources/css/app.css'])
 
 	<!-- Bootswatch Cerulean CSS -->
-	<link href="{{ URL::asset('css/bootstrap.css') }}" rel="stylesheet">
+	<link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet">
 	<!-- Livewire CSS -->
 
 </head>
@@ -31,44 +31,49 @@ $currentYear = Carbon::now()->year;
 	@include('layouts.navbar')
 	<!-- 1st nav end -->
 
-	<!-- 2nd nav -->
-	@include('layouts.navmiddle')
-	<!-- 2nd nav end -->
+	<div class="container-fluid flex-fill d-flex flex-column">
 
-	<div class="container-fluid d-flex flex-fill p-1 mx-auto justify-content-evenly">
+		<!-- 2nd nav -->
+		@include('layouts.navmiddle')
+		<!-- 2nd nav end -->
 
-		<div class="col-sm-2 m-0">
-			<!-- left side menu -->
-			@include('layouts.navleftside')
-			<!-- left side menu end -->
-		</div>
+		<div class="container-fluid p-1 mx-auto d-flex justify-content-between flex-fill">
 
-		<div class="col-sm-8 m-0 my-2 p-1 align-self-center">
-			<div class="col-sm-12">
-				<!-- error message -->
-				@include('layouts.messages')
-				<!-- error message end -->
+			<div class="col-sm-2 m-0">
+				<!-- left side menu -->
+				@include('layouts.navleftside')
+				<!-- left side menu end -->
 			</div>
-		<!-- content -->
-		@isset($slot)
-			<div class="tw">{{ $slot }}</div>
-		@endisset
-		@yield('content')
-		<!-- content end -->
-		</div>
 
-		<div class="col-sm-2 m-0 p-1">
-			<!-- right side info -->
-			@include('layouts.inforightside')
-			<!-- right side info end -->
-		</div>
+			<div class="col-sm-8 m-0 my-2 p-1 align-self-center">
+				<div class="col-sm-12">
+					<!-- error message -->
+					@include('layouts.messages')
+					<!-- error message end -->
+				</div>
+			<!-- content -->
+			@isset($slot)
+				<div class="tw">{{ $slot }}</div>
+			@endisset
+			@yield('content')
+			<!-- content end -->
+			</div>
 
+			<div class="col-sm-2 m-0 p-1">
+				<!-- right side info -->
+				@include('layouts.inforightside')
+				<!-- right side info end -->
+			</div>
+
+
+		</div>
 
 	</div>
 
+
 	<!-- footer -->
 	<div class="container m-0 mx-auto py-1 align-self-end text-center text-sm text-light-emphasis">
-		&copy; My Themes<br />Themes By Bootswatch<br />
+		&copy; {{ config('app.name', 'Laravel') }} {{ $currentYear }}<br />
 		Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
 	</div>
 	<!-- footer end -->
