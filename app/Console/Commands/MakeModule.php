092cabc8c52bdad0d67d4794ce4a409f264e0747
Dhiauddin <dhiauddin@gmail.com>
2026-03-25 07:49:11 +0800

    -- update stubs and make module

---
+++ app/Console/Commands/MakeModule.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class MakeModule extends Command
+{
+	/**
+	 * The name and signature of the console command.
+	 *
+	 * @var string
+	 */
+	// protected $signature = 'app:make-module';
+	protected $signature = 'make:module {name}';
+
+
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	// protected $description = 'Command description';
+	protected $description = 'Generate a full module (model, controller, requests, policy, resource)';
+
+	/**
+	 * Execute the console command.
+	 */
+	public function handle()
+	{
+		$name = $this->argument('name');
+
+		$this->call('make:model', [
+			'name' => $name,
+			'-m' => true 						//migration
+		]);
+
+		$this->call('make:controller', [
+			'name' => "{$name}Controller",
+			'--resource' => true,
+			'--model' => $name
+		]);
+
+		$this->call('make:request', [
+			'name' => "{$this->className($name)}StoreRequest"
+		]);
+
+		$this->call('make:request', [
+			'name' => "{$this->className($name)}UpdateRequest"
+		]);
+
+		$this->call('make:policy', [
+			'name' => "{$this->className($name)}Policy",
+			'--model' => $name
+		]);
+
+		$this->call('make:resource', [
+			'name' => "{$this->className($name)}Resource"
+		]);
+	}
+
+	protected function className($name)
+	{
+		return class_basename($name);
+	}
+
+}
